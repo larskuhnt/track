57413255e92ed510ee65a196a7fb4153e0f0c4a9
Lars Kuhnt <lars.kuhnt@gmail.com>
2011-12-09 11:36:04 +0100

    fetch project skeleton from github

--- example/application.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-require 'track'
-# require 'track/orm/active_record'
-# require 'track/orm/sequel'
-
-Track.boot! File.expand_path('..', __FILE__)
-
-require_relative 'app/controllers/application_controller'
--- example/config.ru
+++
@@ -1,8 +0,0 @@
-require File.join(File.expand_path('..', __FILE__), 'application')
-
-app = Rack::Builder.new do
-  map '/' do
-    run ApplicationController.new
-  end
-end
-run app
